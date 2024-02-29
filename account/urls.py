7eaa5c6260ec88f76ac3e06cb262364c1027f637
Mahbub <mahbuburemon60@gmail.com>
2024-02-29 17:56:36 +0600

    show backend data

--- account/urls.py
+++ account/urls.py
@@ -12,7 +12,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),          # No spaces around the path string
-    path('products/', views.products),   # No spaces around the path string
-    path('customers/', views.customers), # No spaces around the path string
+    path('', views.home, name ='home'),          # No spaces around the path string
+    path('products/', views.products, name ='products'),   # No spaces around the path string
+    path('customers/<str:pk_test>/', views.customers, name ='customers'), # No spaces around the path string
 ]
--- account/views.py
+++ account/views.py
@@ -2,11 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-
+from .models import *
 
 def home(request):
-    return render(request,'account/deshboard.html')
+      orders = Order.objects.all()
+      customers = Customer.objects.all()
+      total_customers = customers.count()
+      total_orders = orders.count()
+      delivered = orders.filter(status='Delivered').count()
+      pending = orders.filter(status='pending').count()
+
+
+    #   context = {'orders':orders, 'customers': customers, 'total_customers':total_customers,'total_orders'= total_orders }
+      context = {'orders':orders, 'customers': customers,
+       'total_customers':total_customers, 'total_orders': total_orders,
+        'delivered':delivered, 'pending':pending}
+
+
+      return render(request,'account/deshboard.html' ,context)
+
 def products(request):
-    return render(request,'account/products.html')
-def customers(request):
-    return render(request,'account/customers.html')
+    products = Products.objects.all()
+    return render(request, 'account/products.html', {'products': products})
+
+def customers(request,pk_test):
+    customer = Customer.objects.get(id=pk_test)
+
+    orders = customer.order_set.all()
+    context = {'customer': customer, 'orders':orders}
+    return render(request,'account/customers.html', context)
